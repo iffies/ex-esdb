c914bff85f367a3ee010f49ae632054d90572e46
beamologist <raf.lefever@erlef.org>
2025-03-20 08:04:29 +0100

    extended Repl

--- system/lib/repl/event_generator.ex
+++ system/lib/repl/event_generator.ex
@@ -4,64 +4,75 @@ defmodule Scarab.Repl.EventGenerator do
   require UUID
 
   @initialized_v1 "initialized:v1"
-  @item_added_v1 "item_added:v1"
-  @item_removed_v1 "item_removed:v1"
-  @money_received_v1 "money_received:v1"
-  @change_returned_v1 "change_returned:v1"
+  
+  @temperature_measured_v1 "temperature_measured:v1"
+  @humidity_measured_v1 "humidity_measured:v1"
+  @light_measured_v1 "light_measured:v1"
+  
+  @fan_acrivated_v1 "fan_acrivated:v1"
+  @fan_deactivated_v1 "fan_deactivated:v1"
+  @light_acrivated_v1 "light_acrivated:v1"
+  @light_deactivated_v1 "light_deactivated:v1"
+  @heater_activated_v1 "heater_activated:v1"
+  @heater_deactivated_v1 "heater_deactivated:v1"
+  @sprinkler_activated_v1 "sprinkler_activated:v1"
+  @sprinkler_deactivated_v1 "sprinkler_deactivated:v1"
+
+  @desired_temperature_set_v1 "desired_temperature_set:v1"
+  @desired_humidity_set_v1 "desired_humidity_set:v1"
+  @desired_light_set_v1 "desired_light_set:v1"
 
   @event_types [
     @initialized_v1,
-    @item_added_v1,
-    @item_removed_v1,
-    @money_received_v1,
-    @change_returned_v1
+ 
+    @temperature_measured_v1,
+    @humidity_measured_v1,
+    @light_measured_v1,
+
+    @desired_temperature_set_v1,
+    @desired_humidity_set_v1,
+    @desired_light_set_v1,
+
+    @light_acrivated_v1,
+    @light_deactivated_v1,
+    @fan_acrivated_v1,
+    @fan_deactivated_v1,
+    @heater_activated_v1,
+    @heater_deactivated_v1,
+    @sprinkler_activated_v1,
+    @sprinkler_deactivated_v1,
   ]
 
-  @customers [
+
+  @operators [
     "John",
     "Paul",
     "George",
     "Ringo",
-    "Pete",
-    "Scott",
-    "Adam",
-    "Eddie",
-    "Fred",
-    "Wilma",
-    "Jane",
-    "Betty",
-    "Helen",
-    "Mary",
-    "Susan"
   ]
 
-  @items [
-    "Apple",
-    "Banana",
-    "Orange",
-    "Grape",
-    "Pear",
-    "Strawberry",
-    "Blueberry",
-    "Pineapple",
-    "Watermelon"
-  ]
-
-  @sizes [
-    "Small",
-    "Medium",
-    "Large",
-    "Extra Large",
-    "Huge"
-  ]
   # Example content type values
   @content_types [1, 2, 3]
 
-  defp random_customer, do: Enum.random(@customers)
-  defp random_item, do: Enum.random(@items)
-  defp random_size, do: Enum.random(@sizes)
-  defp random_quantity, do: :rand.uniform(100)
-  defp random_money, do: :rand.uniform(100)
+  defp random_operator, do: Enum.random(@operators)
+  defp random_temperature, do: 5 + :rand.uniform(30)
+  defp random_humidity, do:  :rand.uniform(100)
+  defp random_light, do:  :rand.uniform(100)
+  defp random_intensity, do:  :rand.uniform(100)
+
+  def initialize(),
+    do:
+    %Scarab.NewEvent{
+      event_id: generate_uuid(),
+      event_type: @initialized_v1,
+      data_content_type: 1,
+      metadata_content_type: 1,
+      data: %{
+       temperature: random_temperature(),
+       humidity: random_humidity(),
+       light: random_light()
+       }
+    }
 
   def generate_events(count)
       when is_integer(count) and count > 0,
@@ -75,53 +86,46 @@ defmodule Scarab.Repl.EventGenerator do
 
     %Scarab.NewEvent{
       event_id: event_id,
-      event_type: Enum.random(@event_types),
+      event_type: event_type,
       data_content_type: Enum.random(@content_types),
       metadata_content_type: Enum.random(@content_types),
-      data: generate_random_data(event_type),
+      data: random_payload(event_type),
       metadata: generate_optional_metadata()
     }
   end
 
-  defp generate_uuid do
-    UUID.uuid4()
-  end
-
-  defp generate_random_data(@initialized_v1),
+  defp generate_uuid, do: UUID.uuid4()
+  defp random_payload(@temperature_measured_v1),
     do: %{
-      customer: random_customer()
+      temperature: random_temperature(),
+    }
+  defp random_payload(@humidity_measured_v1),
+    do: %{
+      humidity: random_humidity(),
     }
 
-  defp generate_random_data(@item_added_v1),
+  defp random_payload(@light_measured_v1),
     do: %{
-      customer: random_customer(),
-      item: random_item(),
-      size: random_size(),
-      quantity: random_quantity(),
-      price: random_money()
+      light: random_light(),
     }
-
-  defp generate_random_data(@item_removed_v1),
+  defp random_payload(@desired_temperature_set_v1),
     do: %{
-      customer: random_customer(),
-      item: random_item(),
-      size: random_size(),
-      quantity: random_quantity(),
-      price: random_money()
+      temperature: random_temperature(),
+      operator: random_operator()
     }
-
-  defp generate_random_data(@money_received_v1),
+  defp random_payload(@desired_humidity_set_v1),
     do: %{
-      customer: random_customer(),
-      amount: random_money(),
-      currency: "USD"
+      humidity: random_humidity(),
+      operator: random_operator()
     }
-
-  defp generate_random_data(@change_returned_v1),
+  defp random_payload(@desired_light_set_v1),
     do: %{
-      customer: random_customer(),
-      amount: random_money(),
-      currency: "USD"
+      light: random_light(),
+      operator: random_operator()
+    }
+  defp random_payload(_),
+    do: %{
+      intensity: random_intensity(),
     }
 
   defp generate_random_bytes(length) do
@@ -132,7 +136,7 @@ defmodule Scarab.Repl.EventGenerator do
   defp generate_optional_metadata do
     # 70% chance of metadata
     if :rand.uniform() < 0.7 do
-      generate_random_bytes(8)
+      generate_random_bytes(128)
     end
   end
 end
--- system/lib/scarab_repl.ex
+++ system/lib/scarab_repl.ex
@@ -5,9 +5,8 @@ defmodule ScarabRepl do
   alias Scarab.Config, as: ScarabConfig
   alias Scarab.EventStore, as: ScarabEventStore
   alias Scarab.Repl.EventGenerator, as: EventGenerator
-  alias Scarab.System, as: ScarabSystem
 
-  #  alias Scarab.EventStreamReader, as: ESReader
+  # alias Scarab.EventStreamReader, as: ESReader
   # alias Scarab.EventStreamWriter, as: ESWriter
 
   alias Scarab.EventStoreInfo, as: ESInfo
@@ -15,46 +14,74 @@ defmodule ScarabRepl do
   require Logger
 
   @scarab_app :scarab_es
-  @store_id :sell_goods_at_pos
-  @stream_name1 :order_stream1
-  @stream_name2 :order_stream2
-  @stream_name3 :order_stream3
-  @stream_name4 :order_stream4
-  @stream_name5 :order_stream5
+  @store :reg_gh
+  @greenhouse1 :greenhouse1
+  @greenhouse2 :greenhouse2
+  @greenhouse3 :greenhouse3
+  @greenhouse4 :greenhouse4
+  @greenhouse5 :greenhouse5
 
-  def store, do: @store_id
-  def stream1, do: @stream_name1
-  def stream2, do: @stream_name2
-  def stream3, do: @stream_name3
-  def stream4, do: @stream_name4
-  def stream5, do: @stream_name5
+  def store, do: @store
+  def stream1, do: @greenhouse1
+  def stream2, do: @greenhouse2
+  def stream3, do: @greenhouse3
+  def stream4, do: @greenhouse4
+  def stream5, do: @greenhouse5
 
   def get_config, do: ScarabConfig.fetch_env!(@scarab_app)
-  def start, do: ScarabSystem.start(get_config())
-  def get_streams, do: ESInfo.get_streams_raw!(@store_id)
+  def get_streams, do: ESInfo.get_streams_raw!(@store)
 
-  def append_events(stream, nbr_of_events) do
-    events =
-      EventGenerator.generate_events(nbr_of_events)
-
+  def initialize(stream) do
+    initialized = EventGenerator.initialize()
     {:ok, actual_version} =
-      @store_id
+      @store
       |> ScarabEventStore.stream_version(stream)
-
     {:ok, new_version} =
-      @store_id
-      |> ScarabEventStore.append_to_stream(stream, actual_version, events)
-
+      @store
+      |> ScarabEventStore.append_to_stream(stream, actual_version, [initialized])
     {:ok, result} =
-      @store_id
+      @store
       |> ScarabEventStore.read_stream_forward(stream, 1, new_version)
-
     {:ok, result, result |> Enum.count()}
   end
 
-  def read_all_events do
-    {:ok, version} = ScarabEventStore.stream_version(@store_id, @stream_name1)
-    {:ok, events} = ScarabEventStore.read_stream_forward(@store_id, @stream_name1, 1, version)
-    events
+
+  def append(stream, nbr_of_events) do
+    case stream |> all() do
+      nil -> 
+        stream 
+        |> initialize()
+        stream
+          |> append(nbr_of_events)
+      _ ->
+        events =
+        EventGenerator.generate_events(nbr_of_events)
+
+        {:ok, actual_version} =
+          @store
+          |> ScarabEventStore.stream_version(stream)
+        {:ok, new_version} =
+          @store
+          |> ScarabEventStore.append_to_stream(stream, actual_version, events)
+        {:ok, result} =
+          @store
+          |> ScarabEventStore.read_stream_forward(stream, 1, new_version)
+      {:ok, result, result |> Enum.count()}
+    end
   end
+
+  def all(stream) do
+    case @store
+         |> ScarabEventStore.stream_version(stream) do
+      {:ok, 0} -> 
+         nil
+
+      {:ok, version} ->    
+         {:ok, events} =              
+            @store               
+            |> ScarabEventStore.read_stream_forward(stream, 1, version)
+            events
+    end
+  end
+
 end
