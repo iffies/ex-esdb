b286990c379c0fa51176f5649f3f749da596408b
beamologist <raf.lefever@erlef.org>
2025-05-19 14:44:45 +0200

    refactored :ex_esdb_filter -> added by_event_pattern

--- system/src/ex_esdb_filter.erl
+++ system/src/ex_esdb_filter.erl
@@ -1,14 +1,14 @@
 -module(ex_esdb_filter).
 
--export([by_stream/1, by_event_type/1]).
+-export([by_stream/1, by_event_type/1, by_event_pattern/1]).
 
 -include_lib("../deps/khepri/include/khepri.hrl").
 
--spec event_type_conditions(EventType :: string()) -> khepri_condition:conditions().
-event_type_conditions(EventType) ->
+-spec event_pattern_conditions(EventPattern :: map()) -> khepri_condition:conditions().
+event_pattern_conditions(EventPattern) ->
   [#if_path_matches{regex = any},
    #if_has_data{has_data = true},
-   #if_data_matches{pattern = #{event_type => EventType}}].
+   #if_data_matches{pattern = EventPattern}].
 
 -spec all_events() -> khepri_condition:conditions().
 all_events() ->
@@ -33,7 +33,11 @@ by_stream(Stream) ->
 
 -spec by_event_type(EventType :: string()) -> khepri:filter().
 by_event_type(EventType) ->
+  by_event_pattern(#{event_type => EventType}).
+
+-spec by_event_pattern(EventPattern :: map()) -> khepri:filter().
+by_event_pattern(EventPattern) ->
   khepri_evf:tree([streams,
                    #if_path_matches{regex = any},
-                   #if_all{conditions = event_type_conditions(EventType)}],
+                   #if_all{conditions = event_pattern_conditions(EventPattern)}],
                   #{on_actions => [create]}).
